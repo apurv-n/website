d84d2597ae4d31daf8ebe081ac99ac146b86d5d7
apurv-n <129954735+apurv-n@users.noreply.github.com>
2024-03-01 14:28:58 +0530

    Update signin.php

--- Code/PHP/signin.php
+++ Code/PHP/signin.php
@@ -1,36 +1,48 @@
 <?php
-  include "connection.php";
-  
-  session_start();
 
-  $regno = $_POST["regno"];
-  $passwd = $_POST["passwd"];
-  $regno = stripcslashes($regno);
-  $password = stripcslashes($passwd);
-  $regno = mysqli_real_escape_string($conn,$regno);
-  $password = mysqli_real_escape_string($conn,$password);
-  $_SESSION['var']=$regno;
+session_start(); 
 
-  $sql = "SELECT * FROM student WHERE reg_no='$regno' and passwd='$password';";
-  $result = mysqli_query($conn, $sql);
+include "connection.php";
 
-  if (mysqli_num_rows($result) > 0) {
-    // output data of each row
-    // echo "User";
-    // while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
-    //   echo "regno: " . $regno. " - Password: " . $row["passwd"]. "Is Registered: " . $row["is_registered"] . "<br>";
-    // }
-    $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
-    // echo "regno: " . $regno. " - Password: " . $row["passwd"]. "Is Registered: " . $row["is_registered"] . "<br>";
+$regno = $_POST["regno"];
+$passwd = $_POST["passwd"];
 
-    if($row["is_registered"] == 1){
-      include "../pages/dashboard.html";
-    }else{
-      include "../pages/semesterRegistration.html";
-    }
+$regno = stripcslashes($regno);
+$password = stripcslashes($passwd);
+
+$regno = mysqli_real_escape_string($conn,$regno);  
+$password = mysqli_real_escape_string($conn,$password);
+
+$sql = "SELECT * FROM student WHERE reg_no='$regno' and passwd='$password'";
+
+$result = mysqli_query($conn, $sql);
+
+if (mysqli_num_rows($result) > 0) {
+
+  $row = mysqli_fetch_assoc($result);
+
+  // Set login session data
+  $_SESSION['user_id'] = $row['id']; 
+  $_SESSION['username'] = $row['name'];
+
+  // Check registration status
+  if($row["is_registered"] == 1){
+    
+    // Redirect to Dashboard HTML
+    header('Location: /Website/Code/HTML/dashboard.html'); 
+    exit;
 
   } else {
-    echo "This is error from Sign in page";
-    include "../pages/dummyError.html";
+
+    // Redirect to Registration HTML
+    header('Location: /Website/Code/HTML/Registration.html');
+    exit;
+  
   }
-?>
+
+} else {  
+
+  // Invalid login  
+  echo "Invalid username or password";
+
+}
